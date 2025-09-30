7d6ab2321c21e3360fd8779dc3045cc1db080fcd
Enes Tekin <enes@Enes-MacBook-Pro.local>
2025-09-30 08:05:18 +0300

    update postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,5 @@
 const config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  plugins: ["@tailwindcss/postcss", "@tailwindcss/typography"],
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
